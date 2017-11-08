2f951d37fab7282a4e04e613069b5ecbe733b3e1
yusa <l.r.play.gvig.wav@gmail.com>
2017-11-08 22:14:11 +0900

    編集機能追加、本文の改行に対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+
   private
     def post_rules
       params.require(:post).permit(:title, :body)
