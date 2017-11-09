4055f9d48a2bb7d41424b4614cae668dd463b251
yusa <l.r.play.gvig.wav@gmail.com>
2017-11-09 21:43:34 +0900

    コメントの追加機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+    def create
+        @post = Post.find(params[:post_id])
+        @post.comments.create(comment_params)
+        redirect_to post_path(@post)
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:body)
+        end
+
 end
