9c5a881383d23a48389879ddef12461127c69da7
yusa <l.r.play.gvig.wav@gmail.com>
2017-11-09 21:15:04 +0900

    コメントの生成、コメントの入力枠設置

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_many :comments
     validates :title, presence: true #, length: { minimum: 3, message: 'Too short to post!' }
     validates :body, presence: true
 end
